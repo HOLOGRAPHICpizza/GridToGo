886df99eee44d2883feb4e3d9a954254955de49f
Jared Hance <jaredhance@gmail.com>
2012-07-25 13:11:13 -0400

    Add configuration generation.
    Log from an example run:
    
    2012-07-25 13:10:28-0400 [-] OpenSim Distribution loading at: /home/jared/.gridtogo/opensim
    2012-07-25 13:10:28-0400 [-] Creating directory: /home/jared/.gridtogo
    2012-07-25 13:10:28-0400 [-] Creating directory: /home/jared/.gridtogo/config
    2012-07-25 13:10:28-0400 [-] Downloading file: /home/jared/.gridtogo/opensim.tar.gz
    2012-07-25 13:10:28-0400 [-] Establishing connection to: dist.opensimulator.org
    2012-07-25 13:10:28-0400 [-] Established connection to: dist.opensimulator.org
    2012-07-25 13:10:28-0400 [-] Requesting file: /opensim-0.7.3.tar.gz
    2012-07-25 13:10:28-0400 [-] Download & Writing file: /home/jared/.gridtogo/opensim-0.7.3.tar.gz
    2012-07-25 13:10:38-0400 [-] Wrote file: /home/jared/.gridtogo/opensim-0.7.3.tar.gz
    2012-07-25 13:10:38-0400 [-] Created symlink: /home/jared/.gridtogo/opensim-0.7.3.tar.gz -> /home/jared/.gridtogo/opensim.tar.gz
    2012-07-25 13:10:38-0400 [-] Extracting file: /home/jared/.gridtogo/opensim.tar.gz
    2012-07-25 13:10:38-0400 [-] Extracted file: /home/jared/.gridtogo/opensim.tar.gz
    2012-07-25 13:10:38-0400 [-] Created symlink: /home/jared/.gridtogo/opensim-0.7.3 -> /home/jared/.gridtogo/opensim
    2012-07-25 13:10:38-0400 [-] OpenSim Distribution loaded at: /home/jared/.gridtogo/opensim
    2012-07-25 13:10:38-0400 [-] Template Info: Grid Name = MyGrid, IP = localhost
    2012-07-25 13:10:38-0400 [-] Template: ./gridtogo/client/opensim/GridCommon.ini -> /home/jared/.gridtogo/opensim/bin/config-include/GridCommon.ini
    2012-07-25 13:10:38-0400 [-] Template: ./gridtogo/client/opensim/Robust.ini -> /home/jared/.gridtogo/opensim/bin/Robust.ini
    2012-07-25 13:10:38-0400 [-] Template: ./gridtogo/client/opensim/OpenSim.ini -> /home/jared/.gridtogo/opensim/bin/OpenSim.ini

--- gridtogo/client/opensim/distribution.py
+++ gridtogo/client/opensim/distribution.py
@@ -2,73 +2,124 @@
 
 import httplib
 import os.path
+import shutil
 import string
 import sys
 import tarfile
 from twisted.python import log
 
-VERSION = "2.7.3"
+VERSION = "0.7.3"
 
 class Distribution(object):
-	def __init__(directory):
+	def __init__(self, projectroot, directory):
+		self.projectroot = projectroot
 		self.directory = directory
 		self.opensimtar = directory + "/opensim.tar.gz"
 		self.opensimdir = directory + "/opensim"
+		self.configdir = directory + "/config"
+		self.load()
 	
 	def load(self):
 		log.msg("OpenSim Distribution loading at: " + self.opensimdir)
 
 		if not os.path.isdir(self.directory):
 			log.msg("Creating directory: " + self.directory)
+			os.mkdir(self.directory)
+
+		if not os.path.isdir(self.configdir):
+			log.msg("Creating directory: " + self.configdir)
+			os.mkdir(self.configdir)
 
 		if not os.path.isdir(self.opensimdir):
-			log.msg("Creating directory: " + self.opensimdir)
-
-		if not os.path.isfile(self.opensimtar):
-			self._download()
-
-		self._extract()
+			if not os.path.isfile(self.opensimtar):
+				self.download()
+			self.extract()
 
 		log.msg("OpenSim Distribution loaded at: " + self.opensimdir)
+	
+	def configure(self, gridname, ip):
+		template = Template(gridname, ip)
+		template.run(
+			self.projectroot + "/gridtogo/client/opensim/GridCommon.ini",
+			self.opensimdir + "/bin/config-include/GridCommon.ini")
+		template.run(
+			self.projectroot + "/gridtogo/client/opensim/Robust.ini",
+			self.opensimdir + "/bin/Robust.ini")
+		template.run(
+			self.projectroot + "/gridtogo/client/opensim/OpenSim.ini",
+			self.opensimdir + "/bin/OpenSim.ini")
 		
-	def _download(self):
+	def download(self):
 		log.msg("Downloading file: " + self.opensimtar)
-		log.msg("Establishing connection to: dist.opensim.org")
-		connection = httplib.HTTPConnection("dist.opensim.org")
-		log.msg("Established connection to: dist.opensim.org")
+		log.msg("Establishing connection to: dist.opensimulator.org")
+		connection = httplib.HTTPConnection("dist.opensimulator.org")
+		log.msg("Established connection to: dist.opensimulator.org")
 
-		log.msg("Requesting file: opensim-" + VERSION + ".tar.gz")
-		connection.request("opensim-" + VERSION + ".tar.gz")
+		log.msg("Requesting file: /opensim-" + VERSION + ".tar.gz")
+		connection.request("GET", "/opensim-" + VERSION + ".tar.gz")
 		response = connection.getresponse()
-		log.msg("Finished downloading: opensim-" + VERSION + ".tar.gz")
+		if response.status != 200:
+			log.err("Bad Response from Server: " + str(response.status))
 
-		log.msg("Writing file: " + self.opensimtar)
-		f = open(self.opensimtar, "w")
+		versionedtar = self.directory + "/opensim-" + VERSION + ".tar.gz"
+		log.msg("Download & Writing file: " + versionedtar)
+		f = open(versionedtar, "w")
 		f.write(response.read())
 		f.close()
-		log.msg("Wrote file: " + self.opensimtar)
-		pass
+		log.msg("Wrote file: " + versionedtar)
+
+		if sys.platform != "win32":
+			os.symlink(versionedtar, self.opensimtar)
+			log.msg("Created symlink: " + versionedtar + " -> " + self.opensimtar)
+		else:
+			log.msg("FileSystem does not allow symlinks, moving directory instead")
+			os.rename(versionedtar, opensimtar)
+			log.msg("Rename: " + versionedsimtar + " -> " + self.opensimtar)
 		
-	def _extract(self):
+	def extract(self):
 		log.msg("Extracting file: " + self.opensimtar)
 		tar = tarfile.open(self.opensimtar)
-		tar.extract(self.opensimdir)
+		tar.extractall(self.directory)
 		tar.close()
 		log.msg("Extracted file: " + self.opensimtar)
-		pass
+
+		olddir = self.directory + "/opensim-" + VERSION
+		newdir = self.directory + "/opensim"
+
+		if sys.platform != "win32":
+			if os.path.isdir(olddir):
+				os.symlink(olddir, newdir)
+				log.msg("Created symlink: " + olddir + " -> " + newdir)
+			else:
+				log.err("Tar file extracted wrong version. Please symlink manually")
+		else:
+			log.msg("FileSystem does not allow symlinks, moving directory instead")
+			os.rename(olddir, newdir)
+			log.msg("Rename: " + olddir + " -> " + newdir)
+
+
 
 class Template(object):
-	def __init__(self, gridname, ip)
+	def __init__(self, gridname, ip):
 		self.gridname = gridname
 		self.ip = ip
+		log.msg("Template Info: Grid Name = " + gridname + ", IP = " + ip)
 	
-	def run(self, fin, fout):
+	def run(self, inloc, outloc):
+		log.msg("Template: " + inloc + " -> " + outloc)
+		fin = open(inloc, "r")
+		fout = open(outloc, "w")
+
 		mappings = {
-			"GRID_NAME": gridname,
-			"IP_ADDRESS": ip
+			"GRID_NAME": self.gridname,
+			"IP_ADDRESS": self.ip
 			}
 		fout.write(string.Template(fin.read()).substitute(mappings))
 
+		fin.close()
+		fout.close()
+
 if __name__ == "__main__":
 	log.startLogging(sys.stdout)
-	Distribution("/home/jared/.gridtogo").load()
+	dist = Distribution(".", "/home/jared/.gridtogo")
+	dist.configure("MyGrid", "localhost")
