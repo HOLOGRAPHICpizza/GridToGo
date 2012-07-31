a695069774cc73cc27355785ee3d8eb18f919d52
Jared Hance <jaredhance@gmail.com>
2012-07-31 15:17:08 -0400

    SHA256 is more standard than SHA224

--- gridtogo/server/authentication.py
+++ gridtogo/server/authentication.py
@@ -13,7 +13,7 @@ class Authenticator(object):
 
 	def authenticateUser(self, loginRequest):
 		#TODO: Flood checking and grid member checking.
-		hashedPassword = hashlib.sha224(loginRequest.password).hexdigest()
+		hashedPassword = hashlib.sha256(loginRequest.password).hexdigest()
 		userAccount = self.database.getUserAccountByName(loginRequest.firstName, loginRequest.lastName)
 		if not userAccount:
 			return UnknownUser(), None
