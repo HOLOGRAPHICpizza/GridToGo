b02dbe45b7b3c2562fedf978337e2b520193666e
Jared Hance <jaredhance@gmail.com>
2012-08-08 08:52:10 -0400

    Implement NAT Client.
    Refs #18.

--- gridtogo/client/nat.py
+++ gridtogo/client/nat.py
@@ -1,6 +1,8 @@
+import socket
 from twisted.internet import reactor
 from twisted.internet.endpoints import TCP4ServerEndpoint
-from twisted.internet.protocol import Protocol
+from twisted.internet.protocol import Protocol, ClientFactory
+from twisted.protocols.basic import LineReceiver
 from twisted.python import log
 
 from gridtogo.shared.networkobjects import *
@@ -66,6 +68,44 @@ class EchoService(object):
 		if self.connectionCount == self.finalCount:
 			deferred.callback(self)
 
+class EchoClient(LineReceiver):
+	def __init__(self, deferred):
+		self.code = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(16))
+		self.deferred = deferred
+		self.timedout = False
+
+	def connectionMade(self):
+		self.sendLine(self.code)
+		reactor.callLater(5, timeout)
+	
+	def lineReceived(self, line):
+		if not self.timedout:
+			if(line == self.code):
+				self.deferred.callback(True)
+			else:
+				self.deferred.callback(False)
+		self.close()
+	
+	def timeout(self):
+		self.deferred.classback(False)
+		self.close()
+
+class EchoClientFactory(ClientFactory):
+	protocol = EchoClient
+
+	def __init__(self, deferred):
+		self.deferred = deferred
+
+	def buildProtocol(self, addr):
+		return EchoClient(deferred)
+
+	def clientConnectionFailed(self, connector, reason):
+		log.msg("NAT Echo Client Connection Failed: " + reason.getErrorMessage())
+		self.deferred.callback(False)
+	
+	def clientConnectionLost(self, connector, reason):
+		log.msg("Connection lost: " + reason.getErrorMessage())
+
 class NATService(object):
 	def __init__(clientObject):
 		self.service = EchoService()
@@ -80,7 +120,31 @@ class NATService(object):
 	def run(self, regionStart, regionEnd):
 		d = Deferred()
 		d.addCallback(self.allEstablished)
-		self.service.start(d, 4 + regionEnd - regionStart)
+		self.count = 4 + regionEnd - regionStart
+		self.tcount = 0
+		self.done = False
+		self.service.start(d, count)
 	
 	def allEstablished(self, ignored):
-		self.clientObject.writeRequest(NATCheckReadyRequest())
+		exthost = socket.gethostbyaddr(socket.gethostname())[0]
+	
+	def resultReceived(self, result):
+		if not self.done:
+			if result:
+				self.tcount += 1
+				if self.tcount == self.count:
+					self.done = True
+					self.success()
+			else:
+				self.done = True
+				self.failure()
+	
+	def success(self):
+		delta = DeltaUser(self.clientObject.localUUID)
+		delta.NATStatus = True
+		self.clientObject.writeRequest(delta)
+
+	def failure(self):
+		delta = DeltaUser(self.clientObject.localUUID)
+		delta.NATStatus = False
+		self.clientObject.writeRequest(delta)
